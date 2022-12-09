bc0fdbf65e3ff9c616787ce8ad693313e5c06572
Albert Chen <33033663+albert-hen@users.noreply.github.com>
2022-12-09 08:35:22 -0500

    better update

--- day9.py
+++ day9.py
@@ -62,32 +62,20 @@ with open(inputfile) as f:
     print(len(visited))
 
 #part 2
+def update3(xdif, ydif):
+    dx,dy = 0,0
 
-def update2(xdif, ydif):
-    tx,ty = 0,0
-    if xdif == 2 and ydif == 0:
-        tx +=1
-    if xdif == -2 and ydif == 0:
-        tx -=1
-    if xdif == 0 and ydif == 2:
-        ty +=1
-    if xdif == 0 and ydif == -2:
-        ty -=1
-
-    if xdif == 2 and ydif == -1 or xdif == 1 and ydif == -2 or xdif == 2 and ydif == -2:
-        tx +=1
-        ty -=1
-    if xdif == -2 and ydif == -1 or xdif == -1 and ydif == -2 or xdif == -2 and ydif == -2:
-        tx -=1
-        ty -=1
-    if xdif == -1 and ydif == 2 or xdif == -2 and ydif == 1 or xdif == -2 and ydif == 2:
-        tx -=1
-        ty +=1
-    if xdif == 1 and ydif == 2 or xdif == 2 and ydif == 1 or xdif == 2 and ydif == 2:
-        tx +=1
-        ty +=1
-
-    return (tx,ty)
+    #find direction
+    if xdif:
+        dx = xdif//abs(xdif)
+    if ydif:
+        dy = ydif//abs(ydif)
+    
+    #only move when either dif is 2
+    if abs(xdif) == 2 or abs(ydif) == 2:
+        return (dx,dy)
+    else:
+        return (0,0)
 
 with open(inputfile) as f:
 
@@ -113,7 +101,8 @@ with open(inputfile) as f:
                 xdif = knots[i-1][0] - knots[i][0]
                 ydif = knots[i-1][1] - knots[i][1]
 
-                txu, tyu = update2(xdif, ydif)
+                txu, tyu = update3(xdif, ydif)
+
 
                 knots[i][0]+=txu
                 knots[i][1]+=tyu
